0376dc7b92b0b32fd9528f6c8c7e035eec52172b
unknown <1.md.rifat.chowdhury@gmail.com>
2023-10-14 20:21:59 +0600

    add

---
+++ src/components/sign/SignUp.jsx
@@ -0,0 +1,29 @@
+
+'use client'
+import React from 'react';
+
+const SignUp = () => {
+    return (
+        <div className='bg-blue-200 p-10'>
+            <div className='flex justify-center'>
+                <form className=''>
+                    <div>
+                        <h2 className='mb-5'>Sign Up</h2>
+                    </div>
+
+                    <div>
+                        <p>Hello</p>
+                    </div>
+                </form>
+                <div>
+                    <h2> hello </h2>
+
+                </div>
+
+            </div>
+            
+        </div>
+    );
+};
+
+export default SignUp;
